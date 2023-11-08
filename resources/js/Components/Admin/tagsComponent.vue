f80131d08a60c7d22a4e0f5c33c1402ce68c248b
JJman <revoded@mail.ru>
2023-11-08 05:09:56 +0300

    input image Components

---
+++ resources/js/Components/Admin/tagsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <link rel="stylesheet" :href="stylePathToTagsComponent">
+    <div class="tagsComponent">
+      
+    </div>
+  </template>
+  
+  <script>
+  
+  export default ({
+    name: "tagsComponent",
+    props: {
+      stylePathToTagsComponent: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+       
+      }
+    },
+  })
+  </script>
+  
+  
